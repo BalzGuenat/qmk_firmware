640479fc2284071585a528931f966aefa0ec35c5
Balz Guenat <balz.guenat@gmail.com>
2021-05-17 20:06:30 +0200

    fix delete word keys

--- keyboards/fc660c/keymaps/coloneljesus/keymap.c
+++ keyboards/fc660c/keymaps/coloneljesus/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,        KC_F8,        KC_F9,  KC_F10, KC_F11, KC_F12, _______,     _______,
         KC_CAPS,KC_MPRV,KC_VOLU,KC_MNXT,KC_PGUP,KC_INS, KC_HOME,LCTL(KC_LEFT),LCTL(KC_RGHT),KC_END, _______,_______,_______,_______,     _______,
         MO(1),  KC_MUTE,KC_VOLD,KC_MPLY,KC_PGDN,KC_DEL, KC_LEFT,KC_DOWN,      KC_UP,        KC_RGHT,_______,_______,     _______,
-        _______,_______,_______,_______,_______,_______,_______,_______,      KC_END,       KC_PGDN,_______,_______,     _______,
+        _______,_______,_______,_______,_______,_______,LCTL(KC_BSPC),LCTL(KC_DEL), _______,_______,_______,_______,     _______,
         _______,_______,_______,                _______,                      _______,      KC_APP, _______,       _______,_______,_______
     ),
   [2] = LAYOUT(
